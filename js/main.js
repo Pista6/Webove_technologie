2bde1372c01aff4df396d3f74e2cf2acc0f84f4b
Stefan Dano <stefandano6@gmail.com>
2018-10-14 12:27:56 +0200

    Add window onload function, basic placeholder for image data

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let galleryData = `{
+    "photos": [
+        {
+          "src": "fotka1.jpg",
+          "title": "Názov fotky 1",
+          "description: "Dlhý popis obrázku 1"
+        },
+        {
+          "src": "fotka2.jpg",
+          "title": "Názov fotky 2",
+          "description": "Dlhý popis obrázku 2"
+        },
+        {
+          "src": "fotka3.jpg",
+          "title": "Názov fotky 3",
+          "description": "Dlhý popis obrázku 3"
+        }      
+      ]
+  }`;
+
+  function init() {}
+
+  window.onload = init;
