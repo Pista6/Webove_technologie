fd77d906d5f35e9729fc2ef5271a274e482c4696
Stefan Dano <stefandano6@gmail.com>
2018-10-14 12:43:43 +0200

    Edit gallery data

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
-let galleryData = `{
-    "photos": [
+const config = {
+    imageFolder: 'images'
+};
+
+let galleryData = {
+    'photos': [
         {
-          "src": "fotka1.jpg",
-          "title": "Názov fotky 1",
-          "description: "Dlhý popis obrázku 1"
+          'src': 'fotka1.jpeg',
+          'title': 'Názov fotky 1',
+          'description': 'Dlhý popis obrázku 1'
         },
         {
-          "src": "fotka2.jpg",
-          "title": "Názov fotky 2",
-          "description": "Dlhý popis obrázku 2"
+          'src': 'fotka2.jpeg',
+          'title': 'Názov fotky 2',
+          'description': 'Dlhý popis obrázku 2'
         },
         {
-          "src": "fotka3.jpg",
-          "title": "Názov fotky 3",
-          "description": "Dlhý popis obrázku 3"
-        }      
+          'src': 'fotka3.jpeg',
+          'title': 'Názov fotky 3',
+          'description': 'Dlhý popis obrázku 3'
+        },
+        {
+          'src': 'fotka4.jpeg',
+          'title': 'Názov fotky 3',
+          'description': 'Dlhý popis obrázku 3'
+        },
+        {
+          'src': 'fotka5.jpeg',
+          'title': 'Názov fotky 3',
+          'description': 'Dlhý popis obrázku 3'
+        }
       ]
-  }`;
+  };
 
   function init() {}
 
